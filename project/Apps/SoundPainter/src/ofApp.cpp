bbcc4756d8b2211f0dff0c7b8e7dd606454b9300
delphi1004 <delphi1004@hanmail.net>
2019-01-23 22:15:58 +0200

    added mp3 files and sound class

--- project/Apps/SoundPainter/src/ofApp.cpp
+++ project/Apps/SoundPainter/src/ofApp.cpp
@@ -1,29 +1,74 @@
 #include "ofApp.h"
 
+
+// private method area ////////////////////////////////////////////////////////////////
+
+void ofApp::initDefaultData()
+{
+    maxBand = 128;
+    
+    mySound.setup(maxBand);
+    
+    mySound.play();
+}
+
+
+
+
+
+// public method area ////////////////////////////////////////////////////////////////
+
+
+
+
 //--------------------------------------------------------------
 void ofApp::setup()
 {
     ofSetWindowShape(ofGetScreenWidth(), ofGetScreenHeight());
     
-    ofSetBackgroundColor(0, 128, 128);
+    ofSetBackgroundColor(30,30,30);
+    
+    ofSetColor(255, 255, 255,10);
+    
+    ofSetBackgroundAuto(false);
+    
+    ofSetLineWidth(0.1);
+    
+    ofBeginSaveScreenAsPDF("screenshot-"+ofGetTimestampString()+".pdf", false);
+    
+    initDefaultData();
 }
 
 //--------------------------------------------------------------
 void ofApp::update()
 {
-
+    mySound.update();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw()
 {
-    ofDrawRectangle(100, 100, 100, 100);
+    float x1,y1,x2,y2;
+    
+    x1 = ofRandom(ofGetScreenWidth());
+    y1 = ofRandom(ofGetScreenHeight());
+    
+    x2 = ofRandom(ofGetScreenWidth()/2);
+    y2 = ofRandom(ofGetScreenHeight()/2);
+    
+    ofDrawLine(x1, y1, x2, y2);
 }
+
+
+// event method area ////////////////////////////////////////////////////////////////
+
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key)
 {
-
+    ofEndSaveScreenAsPDF();
+    
+    ofLog() << "Data saved!";
 }
 
 //--------------------------------------------------------------
